18c5c3c1beddfd7747c3ba7a0d8053096aee8230
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2025-01-08 19:49:41 +0500

    Добавьте файлы проекта.

---
+++ ComicUI.Avalonia.Samples/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComicUI.Avalonia.Samples.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ ComicUI.Avalonia.Samples/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ComicUI.Avalonia.Samples.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
